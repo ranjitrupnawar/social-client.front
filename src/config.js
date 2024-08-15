2a233c2cf3036725307aead025a5098f0d755bfe
Ranjit Rupnawar <162142856+ranjitrupnawar@users.noreply.github.com>
2024-08-15 18:37:21 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "https://urchin-app-v2nci.ondigitalocean.app";
-export const API_BASE_URL = "http://18.143.177.173:6001";
+export const API_BASE_URL = "http://52.77.241.222:6001";
